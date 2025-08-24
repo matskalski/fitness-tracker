6903f511c871f6a739f006990f5041069f71ac1b
Mateusz Skalski <mat.skalski@wp.pl>
2025-08-24 21:40:29 +0200

    zmiana

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
